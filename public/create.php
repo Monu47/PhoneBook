d0dc6ad46dd8709f574e6346b896c846fe89ee32
Monu47 <67874021+Monu47@users.noreply.github.com>
2021-03-24 17:11:00 +0530

    final updated create.php

--- public/create.php
+++ public/create.php
@@ -54,7 +54,7 @@ $statement->execute($new_user);
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Simple Database App</title>
+    <title>PhoneBook</title>
 
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
